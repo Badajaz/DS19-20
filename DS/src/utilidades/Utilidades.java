ef31520143d7f22f44440a96684474e082a2bc30
fc46784 <fc46784@alunos.fc.ul.pt>
2019-12-05 12:17:57 +0000

    imports organizados
    imports organizados

--- DS/src/utilidades/Utilidades.java
+++ DS/src/utilidades/Utilidades.java
@@ -3,19 +3,14 @@ package utilidades;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import java.util.regex.Pattern;
 
 public class Utilidades {
